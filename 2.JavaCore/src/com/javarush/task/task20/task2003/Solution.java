5ceebf689b43e73f0efd13d80cab59d95a286250
ygrek <ygreksh>
2017-09-05 10:13:41 +0300

    qwe

--- 2.JavaCore/src/com/javarush/task/task20/task2003/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2003/Solution.java
@@ -1,6 +1,8 @@
 package com.javarush.task.task20.task2003;
 
 import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 
 /* 
 Знакомство с properties
@@ -8,12 +10,19 @@ import java.io.*;
 public class Solution {
     public static Map<String, String> properties = new HashMap<>();
 
-    public void fillInPropertiesMap() {
+    public void fillInPropertiesMap() throws Exception {
         //implement this method - реализуйте этот метод
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String filename = reader.readLine();
+        reader.close();
+        InputStream inputStream = new FileInputStream(filename);
+        load(inputStream);
+        inputStream.close();
     }
 
     public void save(OutputStream outputStream) throws Exception {
         //implement this method - реализуйте этот метод
+
     }
 
     public void load(InputStream inputStream) throws Exception {
