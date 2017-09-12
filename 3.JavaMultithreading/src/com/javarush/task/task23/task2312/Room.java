e1ded816e8be580bcf1d1920a5b47f5cb8f4247a
ygrek <ygreksh>
2017-09-12 18:23:03 +0300

    qwe

---
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Room.java
@@ -0,0 +1,52 @@
+package com.javarush.task.task23.task2312;
+
+import java.lang.reflect.Modifier;
+
+public class Room {
+    private int width;
+    private int height;
+    private Snake snake;
+    private Mouse mouse;
+
+    public static Room game;
+
+    public Room(int width, int height, Snake snake) {
+        this.width = width;
+        this.height = height;
+        this.snake = snake;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public Snake getSnake() {
+        return snake;
+    }
+
+    public Mouse getMouse() {
+        return mouse;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void setSnake(Snake snake) {
+        this.snake = snake;
+    }
+
+    public void setMouse(Mouse mouse) {
+        this.mouse = mouse;
+    }
+
+    public static void main(String[] args){}
+}
