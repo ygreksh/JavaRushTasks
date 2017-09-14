a4f0bff4092909ec062f5356bd36d71c0defd069
ygrek <ygreksh>
2017-09-14 12:04:18 +0300

    rewrite

---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Arkanoid.java
@@ -0,0 +1,66 @@
+package com.javarush.task.task24.task2413;
+
+import java.util.List;
+import java.util.regex.Pattern;
+
+public class Arkanoid {
+    private int width;
+    private int height;
+    private Ball ball;
+    private Stand stand;
+    private List<Brick> bricks;
+    static Arkanoid game;
+
+    public Ball getBall() {
+        return ball;
+    }
+
+    public void setBall(Ball ball) {
+        this.ball = ball;
+    }
+
+    public Stand getStand() {
+        return stand;
+    }
+
+    public void setStand(Stand stand) {
+        this.stand = stand;
+    }
+
+    public List<Brick> getBricks() {
+        return bricks;
+    }
+
+    public void setBricks(List<Brick> bricks) {
+        this.bricks = bricks;
+    }
+
+    public Arkanoid(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getWidth() {
+
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+    public void run(){}
+    public void move(){}
+
+
+    public static void main(String[] args){
+
+    }
+}
