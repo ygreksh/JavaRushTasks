a1303827fb7778d0453d2d394a83fdaeca6f3532
ygrek <ygreksh>
2017-09-14 17:31:11 +0300

    rewrite

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Stand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Stand.java
@@ -29,6 +29,12 @@ public class Stand extends BaseObject{
 
     @Override
     public void move() {
-
+        x += direction*speed;
+    }
+    public void moveLeft(){
+        direction=-1;
+    }
+    public void moveRight(){
+        direction=1;
     }
 }
