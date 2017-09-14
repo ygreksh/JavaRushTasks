18205aed604a2c2339d76aa94dc16b76b4abf2f2
ygrek <ygreksh>
2017-09-14 17:26:42 +0300

    rewrite

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Stand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Stand.java
@@ -1,8 +1,25 @@
 package com.javarush.task.task24.task2413;
 
 public class Stand extends BaseObject{
+    private double speed;
+    private double direction;
+
     public Stand(double x, double y, double radius) {
         super(x, y, radius);
+    }
+
+    public Stand(double x, double y) {
+        super(x, y, 3);
+        speed = 1;
+        direction = 0;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public double getDirection() {
+        return direction;
     }
 
     @Override
