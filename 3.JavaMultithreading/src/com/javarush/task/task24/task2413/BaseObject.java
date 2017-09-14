34e9e37e3b349387cae0618596995eef54303214
ygrek <ygreksh>
2017-09-14 15:28:36 +0300

    rewrite

---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/BaseObject.java
@@ -0,0 +1,50 @@
+package com.javarush.task.task24.task2413;
+
+public abstract class BaseObject {
+    private double x;
+    private double y;
+    private double radius;
+
+    public BaseObject(double x, double y, double radius) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public abstract void draw(Canvas canvas);
+
+    public abstract void move();
+
+    public boolean isIntersec(BaseObject o){
+        double dx = o.x - this.x;
+        double dy = o.y - this.y;
+        double secdistanse = Math.max(o.radius,this.radius);
+        double currdistanse = Math.sqrt(dx*dx+dy*dy);
+        if (currdistanse<=secdistanse) return true;
+        else return false;
+    }
+}
--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Brick.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Brick.java
@@ -1,4 +1,17 @@
 package com.javarush.task.task24.task2413;
 
-public class Brick {
+public class Brick extends BaseObject{
+    public Brick(double x, double y, double radius) {
+        super(x, y, radius);
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+
+    }
+
+    @Override
+    public void move() {
+
+    }
 }
