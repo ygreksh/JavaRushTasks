f596bbbf0dead1f4be71c84a361bbfaf334c6544
ygrek <ygreksh>
2017-09-14 17:04:55 +0300

    rewrite

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
@@ -54,4 +54,20 @@ public class Canvas {
         }
 
     }
+
+    public void clear(){
+        for (int i=0;i<matrix.length;i++){
+            for (int j=0;j<matrix[0].length;j++){
+                matrix[i][j]=0;
+            }
+        }
+    }
+    public void print(){
+        for (int i=0;i<matrix.length;i++){
+            for (int j=0;j<matrix[0].length;j++){
+                System.out.print(matrix[i][j]);
+            }
+            System.out.println("");
+        }
+    }
 }
