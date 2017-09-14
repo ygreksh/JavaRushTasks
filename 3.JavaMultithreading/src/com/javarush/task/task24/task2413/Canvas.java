35ecddb4ac1462596846a24858c2723527b84062
ygrek <ygreksh>
2017-09-14 16:57:29 +0300

    rewrite

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
@@ -34,4 +34,24 @@ public class Canvas {
     public void setMatrix(char[][] matrix) {
         this.matrix = matrix;
     }
+
+    public void setPoint(double x, double y, char c){
+        int intX = (int) Math.round(x);
+        int intY = (int) Math.round(y);
+        /*
+        if ((intX>=0 && intX<=matrix[0].length) && (intY>=0 && intY<=matrix.length )) {
+            matrix[intX][intY] = c;
+        }*/
+        if (intX<0 || intY<0 || intX>matrix[0].length || intY>matrix.length) return;
+        matrix[intY][intX] = c;
+    }
+
+    public void drawMatrix(double x, double y, int[][] matrix, char c){
+        for (int i=0;i<matrix.length;i++){
+            for (int j=0;j<matrix[0].length;j++){
+                if(matrix[i][j]!=0) setPoint(x+j,y+i,c);
+            }
+        }
+
+    }
 }
