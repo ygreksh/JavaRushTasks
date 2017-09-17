f4d1ba862bb46b1b6b7de5cd38c883f0de391fbb
ygreksh <31619646+ygreksh@users.noreply.github.com>
2017-09-17 10:40:03 +0300

    rewrite

--- 3.JavaMultithreading/src/com/javarush/task/task25/task2505/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2505/Solution.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task25.task2505;
 
-/* 
+
+/*
 Без дураков
 */
 public class Solution {
@@ -15,13 +16,28 @@ public class Solution {
 
         public MyThread(String secretKey) {
             this.secretKey = secretKey;
+
             setUncaughtExceptionHandler(new MyUncaughtExceptionHandler());
+
             setDaemon(true);
         }
 
         @Override
         public void run() {
             throw new NullPointerException("it's an example");
+        }
+
+        private class MyUncaughtExceptionHandler implements UncaughtExceptionHandler {
+
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                try {
+                    Thread.sleep(500);
+                    System.out.println(String.format("%s, %s, %s", secretKey, t.getName(), e.getMessage()));
+                } catch (InterruptedException e1) {
+                    e1.printStackTrace();
+                }
+            }
         }
     }
 
