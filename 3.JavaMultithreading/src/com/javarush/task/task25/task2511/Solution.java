cae22c8609b28b979e78db70ac1788cbe8a83732
ygrek <ygreksh>
2017-09-20 10:05:29 +0300

    rewrite

--- 3.JavaMultithreading/src/com/javarush/task/task25/task2511/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2511/Solution.java
@@ -1,5 +1,6 @@
 package com.javarush.task.task25.task2511;
 
+
 import java.util.TimerTask;
 
 /* 
@@ -14,7 +15,23 @@ public class Solution extends TimerTask {
             throw new NullPointerException();
         }
         this.original = original;
-        this.handler = null;    //init handler here
+        this.handler = new Thread.UncaughtExceptionHandler(){
+
+            @Override
+            public void uncaughtException(Thread thread, Throwable throwable) {
+                String ThreafName = thread.getName();
+                String Message = throwable.getMessage();
+                char[] chars = new char[ThreafName.length()];
+                for (int i=0;i<chars.length;i++){
+                    chars[i]='*';
+                }
+                String newThreadName = String.valueOf(chars);
+                //System.out.println(newThreadName + throwable.getMessage());
+                String newMessage = Message.replaceAll(ThreafName,newThreadName);
+                Exception e = new Exception(newMessage,throwable);
+                System.out.println(e.getMessage());
+            }
+        };     //init handler here
     }
 
     public void run() {
