d31041bf6263a20500079d1101a16653661bc022
ygrek <ygreksh>
2017-09-25 09:26:14 +0300

    rewrite

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -1,5 +1,31 @@
 package com.javarush.task.task30.task3008;
 
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
-    public static void main(String[] args){}
+    private static class Handler extends Thread{
+        public Socket socket;
+
+        public Handler(Socket socket) {
+            this.socket = socket;
+
+        }
+    }
+    public static void main(String[] args) throws IOException {
+
+        int port = ConsoleHelper.readInt();
+        try (ServerSocket serverSocket = new ServerSocket(port)){
+            System.out.println("Сервер запущен");
+            while (true){
+                    Socket socket = serverSocket.accept();
+                    Handler handler = new Handler(socket);
+                    handler.start();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
