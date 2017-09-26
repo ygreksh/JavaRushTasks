c1744774cc2632e77cd9ad98f04eb702382688b1
ygrek <ygreksh>
2017-09-26 15:31:28 +0300

    rewrite

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
@@ -1,10 +1,48 @@
 package com.javarush.task.task30.task3008.client;
 
 import com.javarush.task.task30.task3008.Connection;
+import com.javarush.task.task30.task3008.ConsoleHelper;
+import com.javarush.task.task30.task3008.Message;
+import com.javarush.task.task30.task3008.MessageType;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Client {
     protected Connection connection;
     private volatile boolean clientConnected = false;
+
+    protected String getServerAddress() throws IOException {
+        System.out.println("Введите адрес сервера:");
+        String line = ConsoleHelper.readString();
+        return null;
+    }
+    protected int getServerPort() throws IOException {
+        System.out.println("Введите номер порта сервера:");
+        int port = ConsoleHelper.readInt();
+        return port;
+    }
+    protected String getUserName() throws IOException {
+        System.out.println("Введите имя пользователя:");
+        String userName = ConsoleHelper.readString();
+        return userName;
+    }
+    protected boolean shouldSendTextFromConsole(){
+        return true;
+    }
+    protected SocketThread getSocketThread(){
+        return new SocketThread();
+    }
+    protected void sendTextMessage(String text){
+        try {
+            connection.send(new Message(MessageType.TEXT,text));
+        } catch (IOException e) {
+            //e.printStackTrace();
+            System.out.println("Сообщение не отправлено");
+            clientConnected = false;
+        }
+    }
     public class SocketThread extends Thread{
 
     }
