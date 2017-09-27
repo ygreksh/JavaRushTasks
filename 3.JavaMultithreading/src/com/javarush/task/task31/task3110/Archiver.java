6a479aa12cc94c1b051a96bddb0e076686051f57
ygreksh <31619646+ygreksh@users.noreply.github.com>
2017-09-27 22:07:53 +0300

    rewrite

---
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task31.task3110;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.file.Paths;
+
+public class Archiver {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Введите полный путь к архиву (куда архивировать):");
+        String fullZipPath = reader.readLine();
+        ZipFileManager zipFileManager = new ZipFileManager(Paths.get(fullZipPath));
+        System.out.println("Введите полный путь к файлу (что архивировать):");
+        String fullSourcePath = reader.readLine();
+        zipFileManager.createZip(Paths.get(fullSourcePath));
+    }
+}
