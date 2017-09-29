b6f4fbbd808c4df33eec01465f6102f353d200da
ygreksh <31619646+ygreksh@users.noreply.github.com>
2017-09-29 23:10:45 +0300

    rewrite

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
@@ -1,5 +1,7 @@
 package com.javarush.task.task31.task3110;
 
+import com.javarush.task.task31.task3110.command.ExitCommand;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.nio.file.Paths;
@@ -13,5 +15,7 @@ public class Archiver {
         System.out.println("Введите полный путь к файлу (что архивировать):");
         String fullSourcePath = reader.readLine();
         zipFileManager.createZip(Paths.get(fullSourcePath));
+        ExitCommand exitCommand = new ExitCommand();
+        exitCommand.execute();
     }
 }
--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ConsoleHelper.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ConsoleHelper.java
@@ -5,30 +5,19 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class ConsoleHelper {
-    public static void writeMessage(String message){
+    private static BufferedReader bis = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message) {
         System.out.println(message);
     }
-    public static String readString(){
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String line = null;
-        try {
-            line = reader.readLine();
-            reader.close();
-        } catch (IOException e) {
-            System.out.println("Ошибка ввода строки");
-        }
-        return line;
+
+    public static String readString() throws IOException {
+        String text = bis.readLine();
+        return text;
     }
-    public  static int readInt() {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int number = 0;
-        try {
-            String line = reader.readLine();
-            number = Integer.parseInt(line);
-            reader.close();
-        } catch (IOException e) {
-            System.out.println("Ошибка ввода числа");
-        }
-        return number;
+
+    public static int readInt() throws IOException {
+        String text = readString();
+        return Integer.parseInt(text.trim());
     }
 }
