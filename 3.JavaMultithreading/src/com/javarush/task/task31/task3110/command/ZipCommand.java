e1a0a6eb1a6b3ad301e9a7f09213fde8a1a58be2
ygrek <ygreksh>
2017-10-02 11:40:17 +0300

    rewrite

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipCommand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipCommand.java
@@ -1,4 +1,17 @@
 package com.javarush.task.task31.task3110.command;
 
+import com.javarush.task.task31.task3110.ConsoleHelper;
+import com.javarush.task.task31.task3110.ZipFileManager;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public abstract class ZipCommand implements Command{
+    public ZipFileManager getZipFileManager() throws Exception{
+        String StringZipPath = ConsoleHelper.readString();
+        Path path = Paths.get(StringZipPath);
+        ZipFileManager zipFileManager = new ZipFileManager(path);
+        //zipFileManager.createZip(path);
+        return zipFileManager;
+    }
 }
