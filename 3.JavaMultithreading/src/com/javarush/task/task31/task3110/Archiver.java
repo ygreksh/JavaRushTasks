b5e2830c287c22b9d39fd23a18924fb70e307ff4
ygrek <ygreksh>
2017-10-02 11:14:05 +0300

    rewrite

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
@@ -1,54 +1,37 @@
 package com.javarush.task.task31.task3110;
 
-import com.javarush.task.task31.task3110.command.Command;
-import com.javarush.task.task31.task3110.command.ExitCommand;
 import com.javarush.task.task31.task3110.exception.WrongZipFileException;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.file.Paths;
 
 public class Archiver {
-    public static void main(String[] args) throws Exception {
-        /*
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Введите полный путь к архиву (куда архивировать):");
-        String fullZipPath = reader.readLine();
-        ZipFileManager zipFileManager = new ZipFileManager(Paths.get(fullZipPath));
-        System.out.println("Введите полный путь к файлу (что архивировать):");
-        String fullSourcePath = reader.readLine();
-        zipFileManager.createZip(Paths.get(fullSourcePath));
-        ExitCommand exitCommand = new ExitCommand();
-        exitCommand.execute();
-        */
+    public static void main(String[] args) throws IOException {
+
         Operation operation = null;
-        while(operation != Operation.EXIT){
+        do {
             try {
                 operation = askOperation();
                 CommandExecutor.execute(operation);
-                //if ();
             } catch (WrongZipFileException e) {
                 ConsoleHelper.writeMessage("Вы не выбрали файл архива или выбрали неверный файл.");
-            } catch (Exception e){
+            } catch (Exception e) {
                 ConsoleHelper.writeMessage("Произошла ошибка. Проверьте введенные данные.");
             }
-        }
-    }
-    public static Operation askOperation() throws IOException{
-        ConsoleHelper.writeMessage("Выберите операцию:");
-        ConsoleHelper.writeMessage(Operation.CREATE.ordinal() + " - Создать архив");
-        ConsoleHelper.writeMessage(Operation.ADD.ordinal() + " - Добавить в архив");
-        ConsoleHelper.writeMessage(Operation.EXTRACT.ordinal() + " - Извлечь из архива");
-        ConsoleHelper.writeMessage(Operation.CONTENT.ordinal() + " - Показать содержимое архив");
-        ConsoleHelper.writeMessage(Operation.REMOVE.ordinal() + " - Удалить архив");
-        ConsoleHelper.writeMessage(Operation.EXIT.ordinal() + " - Выход");
 
-        int numberOfCommand = ConsoleHelper.readInt();
-        //Operation operation;
-        for (Operation op : Operation.values()){
-            if (op.ordinal() == numberOfCommand) return op;
-        }
-        return null;
+        } while (operation != Operation.EXIT);
+    }
+
+
+    public static Operation askOperation() throws IOException {
+        ConsoleHelper.writeMessage("");
+        ConsoleHelper.writeMessage("Выберите операцию:");
+        ConsoleHelper.writeMessage(String.format("\t %d - упаковать файлы в архив", Operation.CREATE.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - добавить файл в архив", Operation.ADD.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - удалить файл из архива", Operation.REMOVE.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - распаковать архив", Operation.EXTRACT.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - просмотреть содержимое архива", Operation.CONTENT.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - выход", Operation.EXIT.ordinal()));
+
+        return Operation.values()[ConsoleHelper.readInt()];
     }
 }
--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
@@ -1,9 +1,13 @@
 package com.javarush.task.task31.task3110;
 
 
+import com.javarush.task.task31.task3110.exception.PathIsNotFoundException;
+
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
@@ -14,6 +18,7 @@ public class ZipFileManager {
         this.zipFile = zipFile;
     }
     public void createZip(Path source) throws Exception{
+        /*
         try(ZipOutputStream zipOutputStream = new ZipOutputStream(Files.newOutputStream(zipFile))) {
             ZipEntry zipEntry = new ZipEntry(source.getFileName().toString());
             zipOutputStream.putNextEntry(zipEntry);
@@ -25,6 +30,34 @@ public class ZipFileManager {
             }
             zipOutputStream.closeEntry();
             zipOutputStream.close();
+        }*/
+        Path zipDirectory = zipFile.getParent();
+        if (Files.notExists(zipDirectory))
+            Files.createDirectories(zipDirectory);
+        try (ZipOutputStream zipOutputStream = new ZipOutputStream(Files.newOutputStream(zipFile))) {
+            if (Files.isDirectory(source)) {
+                FileManager fileManager = new FileManager(source);
+                List<Path> fileNames = fileManager.getFileList();
+                for (Path p : fileNames) {
+                    addNewZipEntry(zipOutputStream, source, p);
+                }
+            } else if (Files.isRegularFile(source)) {
+                addNewZipEntry(zipOutputStream, source.getParent(), source.getFileName());
+            } else throw new PathIsNotFoundException();
+        }
+    }
+    private void addNewZipEntry(ZipOutputStream zipOutputStream, Path filePath, Path fileName) throws Exception{
+        Path fullPath = filePath.resolve(fileName);
+        try(InputStream inputStream = Files.newInputStream(fullPath)) {
+            ZipEntry zipEntry = new ZipEntry(fileName.toString());
+            zipOutputStream.putNextEntry(zipEntry);
+            copyData(inputStream, zipOutputStream);
+            zipOutputStream.closeEntry();
+        }
+    }
+    private void copyData(InputStream in, OutputStream out) throws Exception{
+        while (in.available()>0){
+            out.write(in.read());
         }
     }
 }
