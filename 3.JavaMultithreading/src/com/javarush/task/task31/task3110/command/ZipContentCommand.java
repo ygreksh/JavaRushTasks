1c7013e52a27f7263fef5c5715dd5b997c0c405a
ygreksh <31619646+ygreksh@users.noreply.github.com>
2017-10-02 22:15:11 +0300

    rewrite

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipContentCommand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipContentCommand.java
@@ -10,12 +10,16 @@ public class ZipContentCommand extends ZipCommand {
     @Override
     public void execute() throws Exception {
         ConsoleHelper.writeMessage("Просмотр содержимого архива.");
+
         ZipFileManager zipFileManager = getZipFileManager();
-        ConsoleHelper.writeMessage("Содержимое архива.");
+
+        ConsoleHelper.writeMessage("Содержимое архива:");
+
         List<FileProperties> files = zipFileManager.getFilesList();
-        for (FileProperties fileProperties : files){
-            ConsoleHelper.writeMessage(fileProperties.toString());
+        for (FileProperties file : files) {
+            ConsoleHelper.writeMessage(file.toString());
         }
+
         ConsoleHelper.writeMessage("Содержимое архива прочитано.");
     }
 }
