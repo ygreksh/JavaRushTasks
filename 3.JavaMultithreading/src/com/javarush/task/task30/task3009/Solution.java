fa2c653bff02b2dc45ad7ffe852f4eb75c236c3c
ygreksh <31619646+ygreksh@users.noreply.github.com>
2017-10-05 21:13:30 +0300

    rewrite

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3009/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3009/Solution.java
@@ -15,41 +15,18 @@ public class Solution {
         System.out.println(getRadix("1A"));         //expected output: []
     }
     private static Set<Integer> getRadix(String number){
-        Set<Integer> set = new HashSet<>();
-        String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        Set<Integer> result = new HashSet<>();
+        StringBuilder temp;
         try {
-            int num = Integer.parseInt(number);
-            //int l = number.length();
-            //System.out.println("Строка: " + number + ". Число в 10-й системе: " + num);
-            for (int i=2;i<=36;i++){
-                StringBuilder res = new StringBuilder();
-                int n = num;
-                while (n>i) {
-                    int ost = n%i;
-                    if (ost<10) {
-                        res.append(String.valueOf(ost));
-                    }else {
-                        res.append(chars.substring(ost-10,ost-9));
-                        //System.out.println("Добавили " + chars.substring(ost-10,ost-9) + " вместо " + ost);
-                    }
-                    n=n/i;
-                }
-                if (n<10) {
-                    res.append(String.valueOf(n));
-                }else {
-                    res.append(chars.substring(n-10,n-9));
-                    //System.out.println("Добавили " + chars.substring(n-10,n-9) + " вместо " + n);
-                }
-                System.out.println(number + " Число в " + i + "-й системе: " + res.toString() + ". Реверс: " + res.reverse().toString());
-                if (res.toString().equals(res.reverse().toString())){
-                    set.add(i);
-                    //System.out.println(number + " Число в " + i + "-й системе: " + res.reverse().toString() + ". Реверс: " + res.toString());
-                }
+            int numberInDex = Integer.parseInt(number);
+            for (int i = 2; i < 37; i++) {
+                temp = new StringBuilder(Integer.toString(numberInDex, i));
+                if (temp.toString().equals(temp.reverse().toString())) result.add(i);
             }
+
         } catch (NumberFormatException e) {
-            //e.printStackTrace();
         }
 
-        return set;
+        return result;
     }
 }
