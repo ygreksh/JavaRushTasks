5ef6829e3352d399586722ee902cf285b24e1385
ygrek <ygreksh>
2017-10-05 18:00:47 +0300

    rewrite

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3009/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3009/Solution.java
@@ -9,7 +9,7 @@ import java.util.Set;
 
 public class Solution {
     public static void main(String[] args) {
-        System.out.println(getRadix("112"));        //expected output: [3, 27, 13, 15]
+        System.out.println(getRadix("30"));        //expected output: [3, 27, 13, 15]
         System.out.println(getRadix("123"));        //expected output: [6]
         System.out.println(getRadix("5321"));       //expected output: []
         System.out.println(getRadix("1A"));         //expected output: []
@@ -35,10 +35,10 @@ public class Solution {
                     n=n/i;
                 }
                 res.append(n);
+                System.out.println(number + " Число в " + i + "-й системе: " + res.reverse().toString() + ". Реверс: " + res.toString());
                 if (res.toString().equals(res.reverse().toString())){
                     set.add(i);
-                    //System.out.print(res.reverse() + ", ");
-                    //System.out.println(number + " Число в " + i + "-й системе: " + res.toString() + ". Реверс: " + res.reverse().toString());
+                    //System.out.println(number + " Число в " + i + "-й системе: " + res.reverse().toString() + ". Реверс: " + res.toString());
                 }
             }
         } catch (NumberFormatException e) {
