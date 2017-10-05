32615819b2c99c70476c7f0f86ef685b84c9dabd
ygrek <ygreksh>
2017-10-05 11:55:56 +0300

    rewrite

---
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2907/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task29.task2907;
+
+import java.math.BigDecimal;
+
+/* 
+Этот странный BigDecimal
+*/
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(getValue(1.1d, 1.2d));
+    }
+
+    public static BigDecimal getValue(double v1, double v2) {
+        BigDecimal b1 = new BigDecimal(BigDecimal.valueOf(v1).doubleValue());
+        b1 = BigDecimal.valueOf(v1);
+        BigDecimal b2 = new BigDecimal(BigDecimal.valueOf(v2).doubleValue());
+        b2 = BigDecimal.valueOf(v2);
+        b1  = b1.add(b2);
+        return b1;
+    }
+}
