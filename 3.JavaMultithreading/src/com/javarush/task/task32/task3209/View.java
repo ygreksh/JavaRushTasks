239865d91744afb89d5103e1c6383a9819bf5a49
ygreksh <31619646+ygreksh@users.noreply.github.com>
2017-10-07 11:39:43 +0300

    rewrite

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
@@ -1,8 +1,11 @@
 package com.javarush.task.task32.task3209;
 
 import com.javarush.task.task32.task3209.listeners.FrameListener;
+import com.javarush.task.task32.task3209.listeners.TabbedPaneChangeListener;
 
 import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -34,7 +37,17 @@ public class View extends JFrame implements ActionListener {
         controller.exit();
     }
     public void initMenuBar(){}
-    public void initEditor(){}
+    public void initEditor(){
+        htmlTextPane.setContentType("text/html");
+        JScrollPane jScrollPaneHtml = new JScrollPane(htmlTextPane);
+        tabbedPane.addTab("HTML", jScrollPaneHtml);
+        JScrollPane jScrollPaneText = new JScrollPane(plainTextPane);
+        tabbedPane.addTab("Текст", jScrollPaneText);
+        tabbedPane.setPreferredSize(new Dimension(400,400));
+        TabbedPaneChangeListener tabbedPaneChangeListener = new TabbedPaneChangeListener(this);
+        tabbedPane.addChangeListener(tabbedPaneChangeListener);
+        this.getContentPane().add(tabbedPane, BorderLayout.CENTER);
+    }
     public void initGui(){
         initMenuBar();
         initEditor();
