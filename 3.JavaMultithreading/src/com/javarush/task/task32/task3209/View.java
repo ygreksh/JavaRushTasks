5e780be574844b7391c4bc5146f7c4b11e87b131
ygreksh <31619646+ygreksh@users.noreply.github.com>
2017-10-07 09:47:41 +0300

    rewrite

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
@@ -1,5 +1,7 @@
 package com.javarush.task.task32.task3209;
 
+import com.javarush.task.task32.task3209.listeners.FrameListener;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -22,8 +24,21 @@ public class View extends JFrame implements ActionListener {
     public void actionPerformed(ActionEvent actionEvent) {
 
     }
-    public void init(){}
+    public void init(){
+        initGui();
+        FrameListener frameListener = new FrameListener(this);
+        this.addWindowListener(frameListener);
+        setVisible(true);
+    }
     public void exit(){
         controller.exit();
     }
+    public void initMenuBar(){}
+    public void initEditor(){}
+    public void initGui(){
+        initMenuBar();
+        initEditor();
+        pack();
+    }
+
 }
