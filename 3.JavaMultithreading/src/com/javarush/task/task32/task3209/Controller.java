731ba0bd3d241da5fd56326ccad5c1fdc5c52256
ygreksh <31619646+ygreksh@users.noreply.github.com>
2017-10-08 15:47:53 +0300

    rewrite

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
@@ -1,9 +1,12 @@
 package com.javarush.task.task32.task3209;
 
 import javax.swing.event.DocumentListener;
+import javax.swing.text.BadLocationException;
 import javax.swing.text.html.HTMLDocument;
 import javax.swing.text.html.HTMLEditorKit;
 import java.io.File;
+import java.io.IOException;
+import java.io.StringReader;
 
 public class Controller {
     private View view;
@@ -37,5 +40,17 @@ public class Controller {
         document = (HTMLDocument) htmlEditorKit.createDefaultDocument();
         document.addUndoableEditListener(view.getUndoListener());
         view.update();
+    }
+    public void setPlainText(String text){
+        resetDocument();
+        StringReader reader = new StringReader(text);
+        HTMLEditorKit htmlEditorKit = new HTMLEditorKit();
+        try {
+            htmlEditorKit.read(reader,document,0);
+        } catch (IOException e) {
+            ExceptionHandler.log(e);
+        } catch (BadLocationException e) {
+            ExceptionHandler.log(e);
+        }
     }
 }
