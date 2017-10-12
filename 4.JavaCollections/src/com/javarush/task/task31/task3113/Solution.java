c5d1b00539c91e60d1f8e6bcefd2751046e0db72
ygreksh <31619646+ygreksh@users.noreply.github.com>
2017-10-12 23:14:08 +0300

    rewrite

--- 4.JavaCollections/src/com/javarush/task/task31/task3113/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task31/task3113/Solution.java
@@ -1,6 +1,11 @@
 package com.javarush.task.task31.task3113;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /* 
 Что внутри папки?
@@ -8,5 +13,15 @@ import java.io.IOException;
 public class Solution {
 
     public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String line = reader.readLine();
+        Path directory = Paths.get(line);
+        if (!Files.isDirectory(directory)){
+            System.out.println(directory.toString() + " - не папка");
+        }
+        int countDirectories = 0;
+        int countAllFilesAndDirectories = 0;
+        int countSize = 0;
+
     }
 }
