4e7922c006b0c6bc2030bdb1356063dc595c769b
ygrek <ygreksh>
2017-10-13 16:56:17 +0300

    rewrite

---
+++ 4.JavaCollections/src/com/javarush/task/task31/task3109/Solution.java
@@ -0,0 +1,39 @@
+package com.javarush.task.task31.task3109;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+/* 
+Читаем конфиги
+*/
+public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        Properties properties = solution.getProperties("src/com/javarush/task/task31/task3109/properties.xml");
+        properties.list(System.out);
+
+        properties = solution.getProperties("src/com/javarush/task/task31/task3109/properties.txt");
+        properties.list(System.out);
+
+        properties = solution.getProperties("src/com/javarush/task/task31/task3109/notExists");
+        properties.list(System.out);
+    }
+
+    public Properties getProperties(String fileName) {
+        File file = new File(fileName);
+        Properties properties = new Properties();
+        try {
+            if (fileName.endsWith(".xml")){
+                properties.loadFromXML(new FileInputStream(fileName));
+            }else {
+                properties.load(new FileReader(file));
+            }
+        } catch (IOException e) {
+
+        }
+        return properties;
+    }
+}
