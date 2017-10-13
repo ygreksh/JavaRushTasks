9d41b8849139bb0c2f0b37da5207c9e96e6c5bb5
ygrek <ygreksh>
2017-10-13 10:58:34 +0300

    rewrite

--- 4.JavaCollections/src/com/javarush/task/task31/task3105/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task31/task3105/Solution.java
@@ -1,9 +1,12 @@
 package com.javarush.task.task31.task3105;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
 
 /* 
 Добавление файла в архив
@@ -12,11 +15,18 @@ public class Solution {
     public static void main(String[] args) throws IOException {
         String fileName = args[0];
         String zipPath = args[1];
-        FileInputStream zipfile = new FileInputStream(zipPath);
-        ZipInputStream zipInputStream = new ZipInputStream(zipfile);
+        FileInputStream zipInputFile = new FileInputStream(zipPath);
+        ZipInputStream zipInputStream = new ZipInputStream(zipInputFile);
         while (zipInputStream.available() > 0){
             ZipEntry zipEntry = zipInputStream.getNextEntry();
 
         }
+        FileOutputStream zipOutFile = new FileOutputStream(zipPath);
+        ZipOutputStream zipOutputStream = new ZipOutputStream(zipOutFile);
+        zipOutputStream.putNextEntry(new ZipEntry(fileName));
+        File file = new File(fileName);
+
+        zipInputStream.close();
+        zipOutputStream.close();
     }
 }
