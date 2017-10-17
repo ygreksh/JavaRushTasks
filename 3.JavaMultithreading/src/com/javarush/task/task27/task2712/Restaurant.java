4530192166f5bd5e7614ce736890246f8ed0eddf
ygrek <ygreksh>
2017-10-17 10:31:51 +0300

    rewrite

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/Restaurant.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/Restaurant.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task27.task2712;
 
 import com.javarush.task.task27.task2712.kitchen.Cook;
+import com.javarush.task.task27.task2712.kitchen.Waiter;
 
 public class Restaurant {
     public static void main(String[] args){
@@ -8,5 +9,7 @@ public class Restaurant {
         Tablet tablet = new Tablet(5);
         tablet.createOrder();
         tablet.addObserver(cook);
+        Waiter waiter = new Waiter();
+        cook.addObserver(waiter);
     }
 }
