6325b7afcf3e49fe2fe6ac67ece7c22ea351b49e
ygreksh <31619646+ygreksh@users.noreply.github.com>
2017-10-21 10:50:33 +0300

    rewrite

---
+++ 4.JavaCollections/src/com/javarush/task/task32/task3205/CustomInvocationHandler.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task32.task3205;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+public class CustomInvocationHandler implements InvocationHandler {
+    private SomeInterfaceWithMethods someInterfaceWithMethods;
+    public CustomInvocationHandler(SomeInterfaceWithMethods someInterfaceWithMethods) {
+        this.someInterfaceWithMethods = someInterfaceWithMethods;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        Object result;
+        System.out.println(method.getName() + " in");
+        result = method.invoke(someInterfaceWithMethods, args);
+        System.out.println(method.getName() + " out");
+        //result = method.invoke(proxy,args);
+        return result;
+    }
+}
