a36abdcbca2dd9aa730c339f628e649bf9fdc8e2
ygreksh <31619646+ygreksh@users.noreply.github.com>
2017-10-22 13:51:19 +0300

    rewrite

--- 4.JavaCollections/src/com/javarush/task/task22/task2213/Field.java
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Field.java
@@ -23,7 +23,14 @@ public class Field {
         matrix = new int[height][width];
     }
     public void print(){
-
+        for (int i=0; i<height; i++){
+            for (int j=0; j<width; j++){
+                if (matrix[i][j]==0) {
+                    System.out.print(".");
+                }else System.out.print("X");
+            }
+            System.out.println("");
+        }
     }
     public void removeFullLines(){
 
