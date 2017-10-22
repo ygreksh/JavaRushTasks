ae737aefa48c16f29f53cde022dd1d12b159633d
ygreksh <31619646+ygreksh@users.noreply.github.com>
2017-10-22 09:55:36 +0300

    rewrite

--- 4.JavaCollections/src/com/javarush/task/task32/task3211/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task32/task3211/Solution.java
@@ -19,10 +19,23 @@ public class Solution {
     }
 
     public static boolean compareMD5(ByteArrayOutputStream byteArrayOutputStream, String md5) throws Exception {
+        //byte[] buff = byteArrayOutputStream.toByteArray();
+        //String s1 = String.valueOf(buff);
         MessageDigest messageDigest = null;
+        StringBuilder hexString = new StringBuilder();
+        messageDigest = MessageDigest.getInstance("md5");
+        messageDigest.reset();
+        messageDigest.update(byteArrayOutputStream.toByteArray());
+        byte[] newbytes = messageDigest.digest();
+        for (int i = 0; i < newbytes.length; i++) {
+            hexString.append(Integer.toHexString(0xFF & newbytes[i]));
+        }
+        return hexString.toString().equals(md5);
+        /*
         messageDigest = MessageDigest.getInstance("MD5");
         messageDigest.reset();
         messageDigest.update(byteArrayOutputStream.toByteArray());
-        return false;
+        */
+        //return false;
     }
 }
