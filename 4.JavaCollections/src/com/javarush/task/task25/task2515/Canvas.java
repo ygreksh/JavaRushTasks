af18809c5af7ce413965da68dbeb23323fa84d76
ygreksh <31619646+ygreksh@users.noreply.github.com>
2017-10-23 22:35:52 +0300

    rewrite

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/Canvas.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Canvas.java
@@ -8,7 +8,7 @@ public class Canvas {
     public Canvas(int width, int height) {
         this.width = width;
         this.height = height;
-        matrix = new char[height][width];
+        matrix = new char[height+2][width+2];
     }
 
     public int getWidth() {
@@ -21,5 +21,32 @@ public class Canvas {
 
     public char[][] getMatrix() {
         return matrix;
+    }
+    public void setPoint(double x, double y, char c){
+        int intX = (int) Math.round(x);
+        int intY = (int) Math.round(y);
+        if (intX < 0 || intY < 0 || intX > matrix[0].length || intY > matrix.length) return;
+        else matrix[intY][intX] = c;
+    }
+    public void drawMatrix(double x, double y, int[][] matrix, char c){
+        /*
+        //int intX = (int)Math.round(x);
+        //int intY = (int)Math.round(y);
+        for (int i=0; i<matrix.length; i++){
+            for (int j=0; j<matrix[0].length; j++){
+                if (matrix[i][j]==1) {
+                    //this.matrix[j+intX][i+intY] = (char) matrix[j][i];
+                    setPoint(j+x,i+y,c);
+                }
+            }
+        }
+        */
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] != 0) {
+                    setPoint(x + j, y + i, c);
+                }
+            }
+        }
     }
 }
