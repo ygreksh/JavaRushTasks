7ba651a0854026cf35091df6dbe80486e340bb7e
ygrek <ygreksh>
2017-10-23 13:12:11 +0300

    rewrite

--- 4.JavaCollections/src/com/javarush/task/task33/task3308/Shop.java
+++ 4.JavaCollections/src/com/javarush/task/task33/task3308/Shop.java
@@ -1,13 +1,17 @@
 package com.javarush.task.task33.task3308;
 
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
 import java.util.ArrayList;
 import java.util.List;
 
+@XmlType(name = "Shop")
+@XmlRootElement
 public class Shop {
     public Goods goods;
     public int count;
     public double profit;
-    List<String> secretData;
+    public String[] secretData;
 
     static class Goods{
         List<String> names = new ArrayList<>();
