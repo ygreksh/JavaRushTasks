488f56050f65460f021a3eee8cb82a946da7900c
ygreksh <31619646+ygreksh@users.noreply.github.com>
2017-10-25 22:37:41 +0300

    rewrite

--- 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
@@ -1,16 +1,55 @@
 package com.javarush.task.task35.task3513;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Model {
     private static final int FIELD_WIDTH = 4;
     private Tile[][] gameTiles;
 
     public Model() {
-        gameTiles = new Tile[FIELD_WIDTH][FIELD_WIDTH];
+
+        resetGameTiles();
+    }
+    public List<Tile> getEmptyTiles(){
+        List<Tile> emptyTiles = new ArrayList<>();
         for (int i=0;i<FIELD_WIDTH;i++){
+            for (int j=0;j<FIELD_WIDTH;j++){
+                if (gameTiles[i][j].isEmpty()) {
+                    emptyTiles.add(gameTiles[i][j]);
+                }
+            }
+        }
+        return emptyTiles;
+    }
+    public void addTile(){
+
+        List<Tile> emptyTiles = getEmptyTiles();
+        int countOfEmptyTiles = emptyTiles.size();
+        int value;
+        //if(Math.random() < 0.9) value=2;
+        //else value=4;
+        if (emptyTiles != null && countOfEmptyTiles != 0)
+        emptyTiles.get((int) (Math.random()*countOfEmptyTiles)).value = (Math.random() < 0.9 ? 2 : 4);
+
+        /*
+        List<Tile> listForChanges = getEmptyTiles();
+        if (listForChanges != null && listForChanges.size() != 0) {
+            listForChanges.get((int) (listForChanges.size() * Math.random())).value =
+                    (Math.random() < 0.9 ? 2 : 4);
+        }
+        */
+    }
+    public void resetGameTiles(){
+
+        gameTiles = new Tile[FIELD_WIDTH][FIELD_WIDTH];
+        for (int i=0;i<gameTiles.length;i++){
             for (int j=0;j<FIELD_WIDTH;j++){
                 gameTiles[i][j] = new Tile();
             }
         }
+        addTile();
+        addTile();
     }
 }
 
