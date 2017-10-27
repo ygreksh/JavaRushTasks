af9b897cc6e6c9bdfe37a32bc48ed55126d16fa9
ygrek <ygreksh>
2017-10-27 15:51:56 +0300

    rewrite

--- 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
@@ -1,13 +1,31 @@
 package com.javarush.task.task37.task3707;
 
 import java.io.Serializable;
-import java.util.AbstractSet;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.*;
 
-public class AmigoSet<E> extends AbstractSet implements Serializable, Cloneable, Set {
+public class AmigoSet<E> extends AbstractSet<E> implements Serializable, Cloneable, Set<E> {
+    private static final Object PRESENT = new Object();
+    private transient HashMap<E, Object> map;
+
+    public AmigoSet() {
+        map = new HashMap<>();
+    }
+
+    public AmigoSet(Collection<? extends E> collection) {
+        int Capasity = (int) Math.max(16, (collection.size()/.75f+1));
+        map = new HashMap<>(Capasity);
+        for(E iterator : collection){
+            map.put(iterator,PRESENT);
+        }
+    }
+
     @Override
-    public Iterator iterator() {
+    public boolean add(E e) {
+        return null == map.put(e, PRESENT);
+    }
+
+    @Override
+    public Iterator<E> iterator() {
         return null;
     }
 
