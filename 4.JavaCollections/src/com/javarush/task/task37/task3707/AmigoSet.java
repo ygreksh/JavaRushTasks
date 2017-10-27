968320571a30cc786b42a98bb07d0954de744554
ygrek <ygreksh>
2017-10-27 15:58:26 +0300

    rewrite

--- 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
@@ -26,11 +26,15 @@ public class AmigoSet<E> extends AbstractSet<E> implements Serializable, Cloneab
 
     @Override
     public Iterator<E> iterator() {
-        return null;
+        return map.keySet().iterator();
     }
 
     @Override
     public int size() {
-        return 0;
+        return map.size();
     }
+    public boolean isEmpty(){return map.isEmpty();}
+    public boolean contains(Object o){return map.containsKey(o);}
+    public void clear(){map.clear();}
+    public boolean remove(Object o){return null == map.remove(o);}
 }
