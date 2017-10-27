4dde353f2e68e378d4457ec67a50093a667f7876
ygrek <ygreksh>
2017-10-27 17:08:48 +0300

    rewrite

--- 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
@@ -1,5 +1,8 @@
 package com.javarush.task.task37.task3707;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.*;
 
@@ -47,6 +50,23 @@ public class AmigoSet<E> extends AbstractSet<E> implements Serializable, Cloneab
         } catch (Exception e) {
             throw new InternalError();
         }
+
+    }
+    private void writeObject(ObjectOutputStream out) throws IOException {
+        out.defaultWriteObject();
+        out.writeObject(map.keySet());
+        for(E e:map.keySet()){
+            out.writeObject(e);
+        }
+        out.writeFloat(HashMapReflectionHelper.callHiddenMethod(map, "loadFactor"));
+        out.writeInt(HashMapReflectionHelper.callHiddenMethod(map, "capacity"));
+    }
+    private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
+        in.defaultReadObject();
+        Set keys = (Set)in.readObject();
+        float loadFactor = in.readFloat();
+        int capacity = in.readInt();
+        map = new HashMap(capacity, loadFactor);
 
     }
 }
