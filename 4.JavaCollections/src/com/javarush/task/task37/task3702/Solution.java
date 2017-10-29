6177abf689a35755b6cc21c35b4357d259a46224
ygreksh <31619646+ygreksh@users.noreply.github.com>
2017-10-29 16:53:02 +0200

    rewrite

--- 4.JavaCollections/src/com/javarush/task/task37/task3702/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3702/Solution.java
@@ -1,5 +1,15 @@
 package com.javarush.task.task37.task3702;
 
+import com.javarush.task.task37.task3702.male.MaleFactory;
+
 public class Solution {
-    public static void main(String[] args){}
+    public static void main(String[] args){
+        MaleFactory factory = new MaleFactory();
+        Human man1 = factory.getPerson(99);
+        Human man2 = factory.getPerson(4);
+        Human man3 = factory.getPerson(15);
+        System.out.println(man1);
+        System.out.println(man2);
+        System.out.println(man3);
+    }
 }
