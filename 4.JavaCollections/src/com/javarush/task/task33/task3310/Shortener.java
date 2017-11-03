effb7e34fcdac09eb0d64deaddb77abcc7219067
ygrek <ygreksh>
2017-11-03 15:21:17 +0200

    rewrite

--- 4.JavaCollections/src/com/javarush/task/task33/task3310/Shortener.java
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/Shortener.java
@@ -1,10 +1,24 @@
 package com.javarush.task.task33.task3310;
 
+import com.javarush.task.task33.task3310.strategy.StorageStrategy;
+
 public class Shortener {
-    public Long getId(String string){
-        return null;
+    private Long lastId = Long.valueOf(0);
+    private StorageStrategy storageStrategy;
+
+    public Shortener(StorageStrategy storageStrategy) {
+        this.storageStrategy = storageStrategy;
     }
-    public String getString(Long id){
-        return null;
+
+    synchronized public Long getId(String string){
+        if (storageStrategy.containsValue(string)) return storageStrategy.getKey(string);
+        else {
+            lastId ++;
+            storageStrategy.put(lastId,string);
+            return lastId;
+        }
+    }
+    synchronized public String getString(Long id){
+        return storageStrategy.getValue(id);
     }
 }
