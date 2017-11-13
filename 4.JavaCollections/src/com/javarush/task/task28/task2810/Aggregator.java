0e7de296a766428bb1efd6f3cb57291a63cdf055
ygrek <ygreksh>
2017-11-13 17:36:03 +0200

    rewrite

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/Aggregator.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/Aggregator.java
@@ -1,10 +1,16 @@
 package com.javarush.task.task28.task2810;
 
 import com.javarush.task.task28.task2810.model.HHStrategy;
+import com.javarush.task.task28.task2810.model.Model;
 import com.javarush.task.task28.task2810.model.Provider;
+import com.javarush.task.task28.task2810.view.HtmlView;
 
 public class Aggregator {
     public static void main(String[] args){
-
+        HtmlView view = new HtmlView();
+        Provider hhProvider = new Provider(new HHStrategy());
+        Model model = new Model(view,hhProvider);
+        view.setController(new Controller(model));
+        view.userCitySelectEmulationMethod ();
     }
 }
--- 4.JavaCollections/src/com/javarush/task/task28/task2810/view/HtmlView.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/view/HtmlView.java
@@ -9,11 +9,14 @@ public class HtmlView implements View {
     private Controller controller;
     @Override
     public void update(List<Vacancy> vacancies) {
-
+        System.out.println(vacancies.size());
     }
 
     @Override
     public void setController(Controller controller) {
         this.controller = controller;
+    }
+    public void userCitySelectEmulationMethod(){
+        controller.onCitySelect("Odessa");
     }
 }
