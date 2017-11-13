2ef37c5d66d22d658cd6247062e69b027db9c569
ygrek <ygreksh>
2017-11-13 17:31:04 +0200

    rewrite

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/Aggregator.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/Aggregator.java
@@ -5,13 +5,6 @@ import com.javarush.task.task28.task2810.model.Provider;
 
 public class Aggregator {
     public static void main(String[] args){
-        //try {
-        Provider provider = new Provider(new HHStrategy());
-        Controller controller = new Controller(provider);
-            controller.scan();
-    /*
-    } catch (NullPointerException e) {
-            e.printStackTrace();
-        }*/
+
     }
 }
--- 4.JavaCollections/src/com/javarush/task/task28/task2810/Controller.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/Controller.java
@@ -1,5 +1,6 @@
 package com.javarush.task.task28.task2810;
 
+import com.javarush.task.task28.task2810.model.Model;
 import com.javarush.task.task28.task2810.model.Provider;
 import com.javarush.task.task28.task2810.vo.Vacancy;
 
@@ -8,30 +9,15 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Controller {
-    private Provider[] providers;
+    private Model model;
 
-    public Controller(Provider... providers) {
-        this.providers = providers;
-        if (providers.length == 0) throw new IllegalArgumentException();
-    }
-
-    @Override
-    public String toString() {
-        return "Controller{" +
-                "providers=" + Arrays.toString(providers) +
-                '}';
-    }
-
-    public void scan() {
-        List<Vacancy> vacancies = new ArrayList<>();
-
-        try {
-            for (Provider provider : providers) {
-                vacancies.addAll(provider.getJavaVacancies(""));
-            }
-            System.out.println(vacancies.size());
-        } catch (NullPointerException e) {
-            System.out.println(vacancies.size());
+    public Controller(Model model) {
+        if (model == null) {
+            throw new IllegalArgumentException();
         }
+        this.model = model;
+    }
+    public void onCitySelect(String cityName){
+        model.selectCity(cityName);
     }
 }
