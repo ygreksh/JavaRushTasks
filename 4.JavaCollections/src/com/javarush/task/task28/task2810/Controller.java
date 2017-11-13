c65810ac93d479cf1889dc6ff59056b01fd1ce9b
ygrek <ygreksh>
2017-11-13 16:14:31 +0200

    rewrite

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/Controller.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/Controller.java
@@ -1,8 +1,11 @@
 package com.javarush.task.task28.task2810;
 
 import com.javarush.task.task28.task2810.model.Provider;
+import com.javarush.task.task28.task2810.vo.Vacancy;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Controller {
     private Provider[] providers;
@@ -20,6 +23,15 @@ public class Controller {
     }
 
     public void scan() {
-        System.out.println(this.toString());
+        List<Vacancy> vacancies = new ArrayList<>();
+
+        try {
+            for (Provider provider : providers) {
+                vacancies.addAll(provider.getJavaVacancies(""));
+            }
+            System.out.println(vacancies.size());
+        } catch (NullPointerException e) {
+            System.out.println(vacancies.size());
+        }
     }
 }
--- 4.JavaCollections/src/com/javarush/task/task38/task3812/Fox.java
+++
@@ -1,5 +0,0 @@
-package com.javarush.task.task38.task3812;
-
-public class Fox {
-    String name = "fox";
-}
