2a03e0797f941b0981bdaa8b5f4f5587315690e4
ygreksh <31619646+ygreksh@users.noreply.github.com>
2017-11-14 23:15:41 +0200

    rewrite

--- 4.JavaCollections/src/com/javarush/task/task26/task2613/ConsoleHelper.java
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/ConsoleHelper.java
@@ -1,4 +1,27 @@
 package com.javarush.task.task26.task2613;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class ConsoleHelper {
+    private static final BufferedReader bis;
+
+    static {
+        bis = new BufferedReader(new InputStreamReader(System.in));
+    }
+    public static void writeMessage(String message){
+        System.out.println(message);
+    }
+    public static String readString() throws IOException {
+        //BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String line = "";
+        try {
+            line = bis.readLine();
+        } catch (IOException ignored) {
+            //e.printStackTrace();
+            //line = reader.readLine();
+        }
+        return line;
+    }
 }
