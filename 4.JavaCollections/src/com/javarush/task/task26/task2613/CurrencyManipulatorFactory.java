17dbe891511adaee2f77f349b28e94c3855d5815
ygreksh <31619646+ygreksh@users.noreply.github.com>
2017-11-14 23:27:24 +0200

    rewrite

---
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/CurrencyManipulatorFactory.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task26.task2613;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CurrencyManipulatorFactory {
+    private static Map<String, CurrencyManipulator> map = new HashMap<>();
+    private static boolean isExist = false;
+
+    private CurrencyManipulatorFactory()
+    {
+    }
+
+    public static CurrencyManipulator getManipulatorByCurrencyCode(String currencyCode){
+        CurrencyManipulator currencyManipulator;
+
+        if (map.containsKey(currencyCode.toLowerCase()))
+            return map.get(currencyCode.toLowerCase());
+        else {
+            currencyManipulator = new CurrencyManipulator(currencyCode.toLowerCase());
+            map.put(currencyCode.toLowerCase(), currencyManipulator);
+            return currencyManipulator;
+        }
+    }
+}
