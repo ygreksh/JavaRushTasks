49ab08263da0f59fa86fcb0461dd3b70ef201155
ygrek <ygreksh>
2017-11-17 17:24:55 +0200

    rewrite

--- 4.JavaCollections/src/com/javarush/task/task40/task4011/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task40/task4011/Solution.java
@@ -14,6 +14,20 @@ public class Solution {
     }
 
     public static void decodeURLString(String s) throws MalformedURLException {
+        try {
+            URL url = new URL(s);
+            System.out.println(url.getProtocol());
+            System.out.println(url.getAuthority());
+            System.out.println(url.getFile());
+            System.out.println(url.getHost());
+            System.out.println(url.getPath());
+            System.out.println(url.getPort());
+            System.out.println(url.getDefaultPort());
+            System.out.println(url.getQuery());
+            System.out.println(url.getRef());
+        } catch (MalformedURLException e) {
+            System.out.println(String.format("Parameter %s is not a valid URL", s));
+        }
 
     }
 }
