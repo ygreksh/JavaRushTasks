c3de1a55d7576582dd1186fe08e7b850e3e218cf
ygrek <ygreksh>
2017-11-20 17:03:54 +0200

    rewrite

--- 4.JavaCollections/src/com/javarush/task/task34/task3410/model/Model.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/model/Model.java
@@ -1,6 +1,11 @@
 package com.javarush.task.task34.task3410.model;
 
+import com.javarush.task.task34.task3410.controller.EventListener;
+
 public class Model {
     public static final int FIELD_CELL_SIZE = 20;
-
+    private EventListener eventListener;
+    public void setEventListener(EventListener eventListener){
+        this.eventListener = eventListener;
+    }
 }
--- 4.JavaCollections/src/com/javarush/task/task34/task3410/view/Field.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/view/Field.java
@@ -1,5 +1,6 @@
 package com.javarush.task.task34.task3410.view;
 
+import com.javarush.task.task34.task3410.controller.EventListener;
 import com.javarush.task.task34.task3410.model.Box;
 import com.javarush.task.task34.task3410.model.Player;
 
@@ -8,6 +9,7 @@ import java.awt.*;
 
 public class Field extends JPanel {
     private View view;
+    private EventListener eventListener;
 
     public Field(View view) {
         this.view = view;
@@ -18,5 +20,8 @@ public class Field extends JPanel {
 
         Player player = new Player(200, 100);
         player.draw(g);
+    }
+    public void setEventListener(EventListener eventListener){
+        this.eventListener = eventListener;
     }
 }
